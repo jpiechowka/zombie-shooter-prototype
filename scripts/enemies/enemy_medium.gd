7c2256bd8e7eedab98a24e71c77230b7a2c9ba49
Janusz Piechówka <januszpiechowka@gmail.com>
2021-01-06 15:08:37 +0100

    Create medium enemy

---
+++ scripts/enemies/enemy_medium.gd
@@ -0,0 +1,16 @@
+extends RigidBody2D
+
+
+export var enemy_medium_speed_min: int = 75
+export var enemy_medium_speed_max: int = 150
+export var enemy_medium_hp: float = 250.0
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
