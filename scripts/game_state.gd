4fb75a7ce50b2ecd905233c276428449e8c9e3c6
Janusz Piechówka <januszpiechowka@gmail.com>
2021-01-06 02:51:09 +0100

    Use SaverLoader in GameState to save and load game state

--- scripts/game_state.gd
+++ scripts/game_state.gd
@@ -1,16 +1,30 @@
 extends Node
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var game_state: Dictionary = {}
 
 
-# Called when the node enters the scene tree for the first time.
+# Called when the node enters the scene tree for the first time
 func _ready():
-	pass # Replace with function body.
+	# First load saved game state
+	var loaded_game_state = SaverLoader.load_game()
+	if loaded_game_state == null:
+		print_debug("Loaded game state is null indicating that save file has not been created yet")
+		var new_game_state: Dictionary = init_new_game_state()
+		
+		# Save newly initialized game state
+		SaverLoader.save_game(new_game_state)
+	else:
+		print_debug("Successfully loaded game state")
+		game_state = loaded_game_state
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func init_new_game_state() -> Dictionary:
+	print_debug("Initializing new game state")
+	
+	# TODO: initialization
+	var new_game_state = {
+		"game_start_time_epoch" : OS.get_unix_time()
+	}
+	
+	return new_game_state 
