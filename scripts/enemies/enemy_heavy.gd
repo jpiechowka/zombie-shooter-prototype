4bc4f75f163af97d67c8678e0ff35191a3774219
Janusz Piechówka <januszpiechowka@gmail.com>
2021-01-06 21:15:00 +0100

    Add Heavy and Boss enemies

---
+++ scripts/enemies/enemy_heavy.gd
@@ -0,0 +1,16 @@
+extends RigidBody2D
+
+
+export var enemy_heavy_speed_min: int = 50
+export var enemy_heavy_speed_max: int = 125
+export var enemy_heavy_hp: float = 600.0
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
