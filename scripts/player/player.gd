772c41cd0e8d490702cdf868251b5453284c1043
Janusz Piechówka <januszpiechowka@gmail.com>
2021-01-07 16:01:41 +0100

    Add player

---
+++ scripts/player/player.gd
@@ -0,0 +1,15 @@
+extends RigidBody2D
+
+
+export var player_speed: int = 210
+export var player_hp: float = 100.0
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
